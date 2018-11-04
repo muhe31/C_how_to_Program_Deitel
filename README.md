028a1a2987b9fecb779498bf84d6a9ff214efac1
Muhammed Ibrahim <muhe31@gmail.com>
2018-11-03 20:25:15 -0400

    test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CProgramming
 
 This is to help with basic programming and some concepts to pick up in learning how to program in C
+test
+Test 2
--- gas_mileage.c
+++ gas_mileage.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-	
+
 	float gallon;
 	int miles;
 	float answer;
 	printf("Enter the gallon used (-1 to end): ");
 	scanf("%f", &gallon);
-	
+
 	while(gallon != -1){
 		printf("Enter the miles driven: ");
 		scanf("%d", &miles);
-		
+
 		answer = miles/gallon;
 		printf("The miles/gallon for this tank was %f\n\n", answer);
-		
+
 		printf("Enter the gallon used (-1 to end): ");
 		scanf("%f", &gallon);
 	}
+
+	
 }
