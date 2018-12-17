69652b905374f27b83316275f531b599e2e0b17c
Muhammed Ibrahim <muhe31@gmail.com>
2018-12-17 18:29:59 -0500

    comment added

--- chapter3/conditional.c
+++ chapter3/conditional.c
@@ -7,6 +7,7 @@ int main(void){
     float grade = 59.9999;
     printf("%s\n", grade >= 60? "Passed" : "Failed");
 
+    //another to write a conditional statement
     score >= 60? printf("Passed") : printf("Failed");
 
 
