6babe817bc83430c5603eacfaa0abab42c86325c
Muhammed Ibrahim <muhe31@gmail.com>
2018-12-18 18:56:22 -0500

    adding sentinet controlled rep example

---
+++ chapter3/fig03_08.c
@@ -0,0 +1,37 @@
+/*Fig. 3.8: fig03_08.c
+class average program with sentinel
+controlled repetition*/
+
+#include <stdio.h>
+
+int main(void){
+  int counter;
+  int grade;
+  int total;
+
+  float average;
+
+  total = 0;
+  counter = 0;
+
+  printf("Enter grade, -1 to end: ");
+  scanf("%d", &grade);
+
+  while(grade != -1){
+    total = total + grade;
+    counter = counter + 1;
+
+    printf("Enter grade, -1 to end: ");
+    scanf("%d", &grade);
+  }
+
+  if(counter != 0){
+    average = (float)total / counter;
+    printf("Class average is %.2f\n", average);
+  }
+  else{
+    printf("No grades were entered\n");
+  }
+
+  return 0;
+}
