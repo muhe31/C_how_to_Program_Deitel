31ca1e8f05c00c5846b08356af02808dde686bb6
Muhammed Ibrahim <muhe31@gmail.com>
2019-06-08 18:22:40 -0400

    problem 46c

---
+++ chapter3/fig3_46c.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+
+int main(void){
+    
+    int number = 0;
+    int factorial = 1;
+    int backup;
+    int i=1;
+    float sum = 1; 
+    int x;
+
+    printf("enter a positive integer: ");
+    scanf("%d", &number);
+
+    /* Control data input */
+    while (number < 0) {
+	printf("enter a POSITIVE integer: ");
+	scanf("%d", &number);
+    }
+
+    backup = number;
+
+     /* Calculating factorial */
+    while (i <= number) {
+	factorial *= i;
+    sum = sum + (1 / (float)factorial);
+	i++;
+
+    }
+    
+   
+
+    /* printing */
+    printf("%d! = %d\n", backup, factorial);
+    printf("Constant e: %f\n", sum);
+
+    return 0;
+}
