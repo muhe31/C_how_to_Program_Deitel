365b41f7f1d09ac2d538bf04eb8ee9077ef54f7b
Muhammed Ibrahim <hidfigures@gmail.com>
2025-05-10 14:36:44 -0400

    modified fig02_05.c: wrote in two manners ways the add and display sum of two integers uisng printf

--- chapter2/fig02_05.c
+++ chapter2/fig02_05.c
@@ -5,21 +5,23 @@
  /*function main begins program execution*/
  int main ( void ){
 
-   int integer1; //first number to be input by user
-   int integer2; //second number to be input by user
-   int sum; //variable in which sum will be stored
+   int integer1; /*first number to be input by user*/
+   int integer2; /*second number to be input by user*/
+   int sum; /*variable in which sum will be stored*/
 
 
-   printf( "Enter first integer:\n"); //prompt
-   scanf( "%d", &integer1); //read an integer;
+   printf( "Enter first integer:\n"); /*prompt*/
+   scanf( "%d", &integer1); /*read an integer*/
 
-   printf( "Enter second Integer:\n");//prompt
-   scanf( "%d", &integer2); //read an integer1
+   printf( "Enter second Integer:\n"); /*prompt */
+   scanf( "%d", &integer2); /*read an integer1 */
+   printf( "You entered %d and %d\n", integer1, integer2); /*display integers*/
+   printf( "The sum of %d and %d is %d\n", integer1, integer2, integer1 + integer2); /*display sum*/
 
    //assign total to sum
    sum = integer1 + integer2;
    printf( "Sum is %d\n", sum);//print
    printf("Sum is %d\n", integer1 + integer2);
 
-   return 0;
- }
+   return 0; /* indicate that program ended successfully*/
+ } /* end funciton main */
